cc651fc5b679f6090ec5a425df12a3d71ec73a29
Aman Kumar <akprasad8486@gmail.com>
2024-05-01 12:55:40 +0530

    Watched video 1 till 50:00

---
+++ admin/index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <title>Food Order Website   Home page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- Menu section starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+               <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Admin</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="">Food</a></li>
+                <li><a href="">Order</a></li>
+               </ul>
+            </div>
+        </div>
+        <!-- Menu section Ends -->
+        
+
+        <!-- Main content section starts-->
+        <div class="main-content">
+            <div class="wrapper">
+                    Main content goes here
+            </div>
+        </div>
+
+        <!-- Main content section ends-->
+
+        <!-- Footer section starts -->
+        <div class="footer">
+            <div class="wrapper">
+                    <p class="text-center">Aman restaurant Developed by <a href="#">Aman</a></p>
+                </div>
+            </div>
+        <!-- Footer section ends -->
+    </body>
+</html>
